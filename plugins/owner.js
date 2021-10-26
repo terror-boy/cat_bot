fbbc5bc1b834af2e394845a0731504eb2ada08f7
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-10-26 22:57:05 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,1 +1,40 @@
 
+const Julie = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+if (Config.WORKTYPE == 'private') {
+
+    Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+
+        if (message.jid === '15369524516-1612300121@g.us') {
+
+            return;
+        }
+
+        if (Config.OWNER == 'default') {
+            await message.client.sendMessage(message.jid,'*catbot by basil*' , MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
+        }
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+
+        if (message.jid === '54218542512-1612300121@g.us') {
+
+            return;
+        }
+
+        if (Config.OWNER == 'default') {
+            await message.client.sendMessage(message.jid,'*cat_bot by itsmebasil0 💌 basilser*' , MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
+        }
+    }));
+}
